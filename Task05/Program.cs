2ad33ad2a0e0ef0292a1280d1fd3b3b8a730c4a4
Ekaterina <Semenovaekaterinaa@yandex.ru>
2023-05-21 19:34:31 +0700

    Task07

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -6,11 +6,17 @@
 Console.Write("Введите натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+if (number < 1)
+ Console.WriteLine("Некорректный ввод.");
+else
+{
 int i = -number;
 while(i <= number)
+
 {
     Console.Write($"{i} ");
     ++i;
+}
 }
 
 
--- Task07/Program.cs
+++ Task07/Program.cs
@@ -2,4 +2,15 @@
 // а на выходе показывает последнюю цифру этого числа
 // например
 // 456 -> 6
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number % 10;
+    Console.WriteLine($"{lastDigit} является последней цифрой числа {number}");
+}
+else
+{
+    Console.WriteLine("Неверное число.");
+}
