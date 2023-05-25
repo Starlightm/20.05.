7bc9dd34f220d80ccd511189ca43e0bd79efe849
Ekaterina <Semenovaekaterinaa@yandex.ru>
2023-05-25 17:08:12 +0700

    Task 14

---
+++ Task14/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая будет принимать на вход
+//  число и выводить, является  кратным 7 и 23
+// 14 -> нет
+// 46 ->  нет
+// 161 -> да 
+
+// bool DevidedByTwoNumbers(int num, int num1, int num2)
+// {
+//     if ((num % num1 == 0) && (num % num2 == 0)) return true;
+//     else return false;
+// }
+
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// bool myresult = DevidedByTwoNumbers(number, 7, 23);
+// Console.WriteLine(myresult ? "Да" : "Нет");
+
+bool DevidedByTwoNumbers(int num, int num1, int num2)
+{
+    return num % num1 == 0 && num % num2 == 0;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool myresult = DevidedByTwoNumbers(number, 7, 23);
+Console.WriteLine(myresult ? "Да" : "Нет");
