5e592afb29b62dc43bc5f3d0a172f434c64e3462
Ekaterina <Semenovaekaterinaa@yandex.ru>
2023-06-01 16:17:11 +0700

    Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -5,3 +5,33 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ 
+int firstDigitNum = number / 10000;
+int secondDigitNum = number / 1000;
+int fourthDigitNum = (number / 100) % 10;
+int lastDigitNum = number % 10;
+
+void Palindrome(int number);
+if  (number < 10000 && number > 99999) 
+{
+    if (firstDigitNum == lastDigitNum && secondDigitNum == fourthDigitNum)
+    {
+        Console.WriteLine ($"{number} является палиндромом");
+    }
+     else 
+    {
+        Console.WriteLine ($"{number} не является палиндромом");
+    }
+}
+    else 
+    { 
+        Console.WriteLine("Введeно не пятизначное число");
+    }
+
+int num = int.Parse(Console.ReadLine()!);
+Palindrome(number);
+ 
