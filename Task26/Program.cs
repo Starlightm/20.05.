5d1d6dba8d2b4bf230da02378f48194f9dc2a888
Ekaterina <Semenovaekaterinaa@yandex.ru>
2023-06-02 17:15:55 +0700

    task26

---
+++ Task26/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 26
+// Напишите программу, которая принимает нп вход 
+// число и выдает количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5 
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int CountDigit(int num)
+{
+    int digit = 0;
+
+    if (num == 0) return 1;
+    while (num !=0)
+    {
+        digit++;
+        // пока num не равно нулю мы каждую 
+        // итерацию добавляем единицу к digits и отсекаем последнюю цифру num
+        num /=10;
+    }
+    return digit;
+}
+Console.WriteLine($"{number} contains {CountDigit(number)} digit");
