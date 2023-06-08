2816a2215c1d86b3e0e075c5ccf7fc3ba72338e7
Ekaterina <Semenovaekaterinaa@yandex.ru>
2023-06-08 22:00:52 +0700

    Task36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,4 +1,46 @@
-﻿// Задача 36: Задайте одномерный массив, заполненный случайными
-//  числами. Найдите сумму элементов, стоящих на нечётных позициях.
+﻿//  Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("");
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine($"{arr[arr.Length - 1]}]");
+}
+
+
+
+int SumOddElem(int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int[] array = CreateArrayRndInt(10, 1, 10);
+PrintArray(array);
+
+
+Console.WriteLine($" Сумма элементов, стоящих на нечётных позициях = {SumOddElem(array)}");
+
