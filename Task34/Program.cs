1de7e34b6e2b3edda6645708d35a321de6c8251a
Ekaterina <Semenovaekaterinaa@yandex.ru>
2023-06-08 19:00:01 +0700

    Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,45 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("");
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine($"{arr[arr.Length - 1]}]");
+}
+
+int CountEvenDig(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) 
+        count += 1;
+    }
+    return count;
+}
+
+
+
+int[] array = CreateArrayRndInt(11, 100, 999);
+PrintArray(array);
+
+Console.WriteLine($" -> {CountEvenDig(array)}");
+
