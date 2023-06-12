52a4e115406c755dd20909336f1907926767626d
Ekaterina <Semenovaekaterinaa@yandex.ru>
2023-06-12 21:18:30 +0700

    Task43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -5,3 +5,30 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 // найти значение X
+
+double GetNumber(string prompt)
+{
+    Console.Write($"Enter {prompt} value: ");
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+
+void GetintersectionPoint(double k1, double b1, double k2, double b2)
+{
+    if (k1 == k2)
+    {
+        Console.WriteLine("The lines are parallel");
+        return;
+    }
+
+    double x = Math.Round((b2 - b1) / (k1 - k2), 3); // formula for x is (b2 - b1) / (k1 - k2)
+    double y = Math.Round(k1 * x + b1, 3); // formula for y is k1 * x + b1
+    Console.WriteLine($"The intersection point is ({x}, {y})");
+}
+
+double k1 = GetNumber("k1");
+double b1 = GetNumber("b1");
+double k2 = GetNumber("k2");
+double b2 = GetNumber("b2");
+
+GetintersectionPoint(k1, b1, k2, b2);
