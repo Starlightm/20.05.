4460f3ae2262b1f0ed6d5be9d56bca18fd1ac090
Ekaterina <Semenovaekaterinaa@yandex.ru>
2023-06-12 18:02:23 +0700

    Task44/45

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 40: Напишите программу, которая принимает на вход три
+﻿// Задача *40: Напишите программу, которая принимает на вход три
 // числа и проверяет, может ли существовать треугольник с сторонами
 // такой длины.
 // Теорема о неравенстве треугольника: каждая сторона треугольника
@@ -6,7 +6,7 @@
 
 int GetNumber()
 {
-    Console.WriteLine(" Введите число: ");
+    Console.WriteLine("Введите число: ");
     int number = Convert.ToInt32(Console.ReadLine());
     if (number > 0) return number;
     else return 0;
@@ -14,9 +14,8 @@ int GetNumber()
 
 bool SuchTriangleExists(int a, int b, int c)
 {
-if ((a < b + c) && (b < a + c) && (c < a + b))
-return true;
-else return false;
+    if ((a < b + c) && (b < a + c) && (c < a + b)) return true;
+    else return false;
 }
 
 int side1 = GetNumber();
@@ -25,7 +24,7 @@ int side3 = GetNumber();
 
 if (side1 == 0 || side2 == 0 || side3 == 0)
 {
- Console.WriteLine("Некорректный ввод");
- return;
+    Console.WriteLine("Некорректный ввод");
+    return;
 }
 Console.WriteLine(SuchTriangleExists(side1, side2, side3) ? "Такой треугольник существует" : "Такого треугольника не существует");
