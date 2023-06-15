4b3a551ea9fcda3848f0bdf471e3224108719bb8
Ekaterina <Semenovaekaterinaa@yandex.ru>
2023-06-15 18:28:04 +0700

    Task52

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -8,4 +8,63 @@
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 
-Print array.Double math.round
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) 
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+             Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+void PrintArrayDouble(double[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}; ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}");
+}
+
+double[] MidColumns(int[,] matrix)
+{
+    double[] arr = new double[matrix.GetLength(1)];
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            sum += matrix[j, i]; 
+        }
+        arr[i] = Math.Round(sum/matrix.GetLength(0), 2);
+        sum = 0;
+    }
+    return arr ;
+}
+
+
+int[,] matrix = CreateMatrixRndInt(3, 4, 1, 20);
+PrintMatrix(matrix);
+double[] array = MidColumns(matrix);
+
+
+Console.WriteLine();
+Console.Write("Среднее арифметическое каждого столбца: ");
+PrintArrayDouble(array);
